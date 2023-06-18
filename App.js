da857af4088f1456dc673df02ad67f00c092d0bc
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-06-18 10:22:02 +0530

    native stack navigator added

--- App.js
+++ App.js
@@ -1,27 +1,33 @@
-import {StatusBar} from "expo-status-bar";
+import { StatusBar } from "expo-status-bar";
 import React from "react";
-import {StyleSheet} from "react-native";
+import { StyleSheet } from "react-native";
 import CategoryScreen from "./screens/CategoryScreen";
-import {NavigationContainer} from "@react-navigation/native";
+import { NavigationContainer } from "@react-navigation/native";
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-    return (
-        <>
-            <StatusBar style='light'/>
-            <NavigationContainer>
-                <CategoryScreen/>
-            </NavigationContainer>
-        </>
-    );
+  return (
+    <>
+      <StatusBar style="light" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="MealsCategories" component={CategoryScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-        // padding: 16, // Apply padding to the container
-        // marginTop: 100, // Apply margin to the top
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    // padding: 16, // Apply padding to the container
+    // marginTop: 100, // Apply margin to the top
+  },
 });
